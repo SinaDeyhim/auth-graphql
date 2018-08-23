6743cb89492fe5f2296c18d527b07ed71b77fddd
SinaDeyhim <sina.deyhim@gmail.com>
2018-08-23 16:34:03 -0400

    finished

--- client/index.js
+++ client/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface} from 'apollo-client'; //backend
+import { ApolloProvider } from 'react-apollo'; // glue btw apollo and react
+import { Router, hashHistory, Route, IndexRoute} from 'react-router';
+import App from './components/App';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
+import requireAuth from './components/requireAuth';
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts:{
+    credentials: 'same-origin'
+  }
+});
+const client = new ApolloClient({
+  networkInterface,
+  // to store the fetched data in a cache
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history ={hashHistory}>
+        <Route path="/" component ={App}>
+          <Route path="login" component ={LoginForm}/>
+          <Route path="signup" component ={SignupForm}/>
+          <Route path="dashboard" component ={requireAuth(Dashboard)}/>
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://Sina:de136357@ds227332.mlab.com:27332/authtenticate';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
